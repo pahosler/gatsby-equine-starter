fb85802abf0f8b11cddee6590fb28a09b8ec05d1
Paul Hosler <pahosler@gmail.com>
2019-07-23 11:47:48 -0500

    hompage preview bugfix(5)

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: 'Equine Advantage',
   siteTitleAlt: 'Equine Advantage', // for SEO
-  siteLogo: '/icons/naeed-a-logo.png', // Logo used for SEO and manifest.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://equineadvantage.com',
   pathPrefix: '/',
   siteDescription: 'Super food for horses', // Website description used for RSS feeds/meta description tag.
