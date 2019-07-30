bee5f252dbd371a2d77f20ec442390f8e3935f80
Paul Hosler <pahosler@gmail.com>
2019-07-30 00:57:04 -0500

    bugfix home-preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,14 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: `/assets/img/`,
+        },
+      },
+    },
+    {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
