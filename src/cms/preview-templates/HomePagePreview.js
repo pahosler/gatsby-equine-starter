092c36f6bc1d5efd56376afe139dde26f9244b38
Paul Hosler <pahosler@gmail.com>
2019-07-29 22:40:57 -0500

    bugfix home-preview

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -8,9 +8,6 @@ const HomePagePreview = ({ entry, getAsset }) => {
   const entryPartnerLogo = entry.getIn(['data', 'partners', 'logo'])
   const logo = entryPartnerLogo ? entryPartnerLogo.toJS() : []
 
-  const entryAdvantage = entry.getIn(['data', 'advantage'])
-  const advantage = entryAdvantage ? entryAdvantage.toJS() : []
-
   return (
     <HomePageTemplate
       title={entry.getIn(['data', 'title'])}
@@ -26,7 +23,7 @@ const HomePagePreview = ({ entry, getAsset }) => {
         name: entry.getIn(['data', 'quote', 'name']),
         title: entry.getIn(['data', 'quote', 'title']),
       }}
-      advantage={{ advantage }}
+      advantage={ entry.getIn(['data','advantage']}
     />
   )
 }
