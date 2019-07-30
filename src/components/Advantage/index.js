c5bebd37b2bd6e765c0d3fd172a9193ddcd14941
Paul Hosler <pahosler@gmail.com>
2019-07-30 01:35:56 -0500

    bugfix home-preview

--- src/components/Advantage/index.js
+++ src/components/Advantage/index.js
@@ -10,14 +10,14 @@ const Advantage = ({ advantage }) => (
     </div>
     <div className='column is-three-fifths is-offset-one-fifth'>
       <div className='columns is-multiline is-centered'>
-        {advantage.map(item => (
-          <div key={item.image} className='column is-one-third'>
+        {advantage.map(({ image, text }) => (
+          <div key={image} className='column is-one-third'>
             {/* <div className='level-item'> */}
             {/* <figure className='image is-128x128' > */}
-            <img alt={item.name} src={item.image} style={{ margin: 50 }} />
+            <img alt={image} src={image} style={{ margin: 50 }} />
             {/* </figure> */}
             {/* </div> */}
-            <p className='is-size-5 has-text-ea-black has-text-centered' style={{ paddingTop: 20 }}>{item.text}</p>
+            <p className='is-size-5 has-text-ea-black has-text-centered' style={{ paddingTop: 20 }}>{text}</p>
           </div>
         ))}
       </div>
