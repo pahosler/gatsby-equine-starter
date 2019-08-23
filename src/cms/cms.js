496f74efb747ca590fc7732950dfb63c8bb4280c
Paul Hosler <pahosler@gmail.com>
2019-08-23 11:51:50 -0500

    changes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,13 +3,8 @@ import '../assets/sass/styles.sass'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import ProductsPreview from './preview-templates/ProductsPreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
-import StoreLocatorPagePreview from './preview-templates/StoreLocatorPagePreview'
+// import StoreLocatorPagePreview from './preview-templates/StoreLocatorPagePreview'
 
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('products', ProductsPreview)
-<<<<<<< HEAD
-=======
-CMS.registerPreviewTemplate('highlights', HighlightsPagePreview)
-CMS.registerPreviewTemplate('locator', StoreLocatorPagePreview)
->>>>>>> ea5bf32467ad047a9b48daa012883a917b3ceca5
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
